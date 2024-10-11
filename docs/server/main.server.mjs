7b2416404acfd6b04cec98322b04c1a3034c9d49
slava-danilchak <danilchakv@gmail.com>
2024-10-11 10:18:21 +0300

    Add build to the /docs folder

---
+++ docs/server/main.server.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{a}from"./chunk-RW4557OF.mjs";import"./chunk-EAQY4GKZ.mjs";import"./chunk-WDCV6XWC.mjs";import"./chunk-FDSLRJ4U.mjs";import"./chunk-PJZQDYVZ.mjs";import"./chunk-J27SCW2U.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
